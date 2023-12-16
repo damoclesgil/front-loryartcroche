3ebe64fd5b1d4e07119b232568bf951ef094750a
damoclesgil <damoclesgil@gmail.com>
2023-12-16 00:02:05 -0300

    Feat: add Speed Insights Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Menu from '@/components/Menu'
-import Footer from '@/components/Footer'
 import Providers from './providers'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const inter = Inter({ subsets: ['latin'], display: 'swap' })
 
@@ -21,6 +20,7 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body className={inter.className}>
         <Providers>{children}</Providers>
+        <SpeedInsights />
       </body>
     </html>
   )
