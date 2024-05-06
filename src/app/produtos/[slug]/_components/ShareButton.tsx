aa97c98a6c960801aad6ba2b14becd2a81eca712
damoclesgil <damoclesgil@gmail.com>
2024-05-05 21:50:40 -0300

    Fix: the carniça

---
+++ src/app/produtos/[slug]/_components/ShareButton.tsx
@@ -0,0 +1,34 @@
+import formatPrice from '@/utils/format-price'
+import { Share1Icon } from '@radix-ui/react-icons'
+import { Produto } from '@/utils/types/Produto.type'
+
+type shareButtonProps = {
+  product: Produto
+}
+
+export function ShareButton({ product }: shareButtonProps) {
+  const shareProduct = async () => {
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: product.nome,
+          text: `Check out this product: ${product.nome} - ${formatPrice(Number(product.preco))}`,
+          url: window.location.href
+        })
+        console.log('Product shared successfully')
+      } catch (error) {
+        console.error('Error sharing product:', error)
+      }
+    }
+  }
+
+  return (
+    <button
+      className="mb-2 text-left flex items-center"
+      onClick={() => shareProduct()}
+    >
+      <Share1Icon className="w-4 h-4 mr-2 " />
+      <span>Compartilhar</span>
+    </button>
+  )
+}
